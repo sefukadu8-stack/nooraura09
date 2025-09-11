5f95267d5208989506af5ee848d62f167e5b58af
sefukadu8-stack <sefukadu8@gmail.com>
2025-09-11 13:42:18 +0530

    Add files via upload

---
+++ client/constants.ts
@@ -0,0 +1,26 @@
+export const WHATSAPP_NUMBER = "918780813692";
+export const COMPANY_EMAIL = "sefudinkadu@gmail.com";
+
+export const PRODUCT_CATEGORIES = [
+  { value: "all", label: "All" },
+  { value: "cotton", label: "Cotton" },
+  { value: "silk", label: "Silk" },
+  { value: "chiffon", label: "Chiffon" },
+  { value: "designer", label: "Designer" }
+];
+
+export const PRODUCT_SIZES = ["S", "M", "L", "XL", "XXL"];
+
+export const INDIAN_STATES = [
+  "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
+  "Delhi", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand",
+  "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur",
+  "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan",
+  "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh",
+  "Uttarakhand", "West Bengal"
+];
+
+export const PAYMENT_MODES = [
+  { value: "cod", label: "Cash on Delivery (COD)" },
+  { value: "prepaid", label: "Prepaid (Online Payment)" }
+];
